3b7652ac355d727bf82629168afafe532c903e61
IDF <d.islamgulov@gmail.com>
2017-11-13 14:58:35 +0600

    Add arguments

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import os
+import sys
 import time
-import requests 
+#import requests 
 import urllib.request
 import threading
 from threading import Thread
@@ -16,34 +17,43 @@ class OpenURL(Thread):
         self.url = url
     
     def run(self):
-        handle = urllib.request.urlopen(self.url).read()
+        handle = urllib.request.urlopen(self.url,timeout=3000).read()
         #handle = requests.get(self.url)
         msg = "%s закончил загрузку %s!" % (self.name, self.url)
         print(msg)
  
 #while i<=50:
+#Первый аргумент определяет кол-во магазинов, если не указан по умолчанию выставляется 20
+if len(sys.argv)>1:
+   nend = sys.argv[1]
+else:
+   nend = 20
+#Второй аргумент определяет кол-во категорий товаров, если не указан по умолчанию выставляется 100
+if len(sys.argv)>2:
+   iend = sys.argv[2]
+else:
+   iend = 100
 while True:
   urls = []
   n=0
   sid="?id="+str(i)+"&"
-  with open('pars.txt', encoding='utf-8') as f: 
+  with open('list.txt', encoding='utf-8') as f: 
     for line in f:
        if sid in line:
           line = line.splitlines()
           urls.extend(line)
           n+=1
-       if n>=19:
+       if n>=int(nend):
           break	 
   i+=1
-  #i+=50
   u=1
   for url in urls:
     name = "Thread num: %s" % str(u)
     thread = OpenURL(url, name)
     thread.start()
     u+=1
-  time.sleep(600)
-  if i==50:
+  time.sleep(300)
+  if i==iend:
      i=0
-     print(Start parsing again!)
+     print('Start parsing again!')
 
