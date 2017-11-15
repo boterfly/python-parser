75188c4d5c70449f242a198b6b67737c26284f1e
IDF <d.islamgulov@gmail.com>
2017-11-15 17:18:19 +0600

    Add exclude categories

--- parser.py
+++ parser.py
@@ -8,7 +8,7 @@ import threading
 from threading import Thread
 
 i=1
-
+exshops = [5,19,21,44,55]
 class OpenURL(Thread):
     
     def __init__(self, url, name):
@@ -18,9 +18,9 @@ class OpenURL(Thread):
     
     def run(self):
         handle = urllib.request.urlopen(self.url,timeout=3000).read()
-        #handle = requests.get(self.url)
         msg = "%s закончил загрузку %s!" % (self.name, self.url)
         print(msg)
+	
  
 #while i<=50:
 #Первый аргумент определяет кол-во магазинов, если не указан по умолчанию выставляется 20
@@ -37,23 +37,26 @@ while True:
   urls = []
   n=0
   sid="?id="+str(i)+"&"
-  with open('list.txt', encoding='utf-8') as f: 
-    for line in f:
-       if sid in line:
-          line = line.splitlines()
-          urls.extend(line)
-          n+=1
-       if n>=int(nend):
-          break	 
+  if i not in exshops:
+    with open('list.txt', encoding='utf-8') as f: 
+       for line in f:
+         if sid in line:
+           line = line.splitlines()
+           urls.extend(line)
+           n+=1
+         if n>=int(nend):
+           break	 
   i+=1
   u=1
-  for url in urls:
-    name = "Thread num: %s" % str(u)
-    thread = OpenURL(url, name)
-    thread.start()
-    u+=1
-  time.sleep(300)
-  if i==iend:
-     i=0
-     print('Start parsing again!')
+  if urls:
+    for url in urls:
+      name = "Thread num: %s" % str(u)
+      thread = OpenURL(url, name)
+      thread.start()
+      if (int(u) % 2 == 0):
+        time.sleep(300)
+      u+=1
+    if i==iend:
+      i=0
+      print('Start parsing again!')
 
